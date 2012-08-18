273643601935336b3c5e1b621a812a4ef688cc8d
Dean Perry <deanpcmad@gmail.com>
2012-08-18 21:28:16 +0100

    oops

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "OKFyfNHLcwpqx5I91Qkvgg", "XA1ramUNLoj9iuJNJdJZXIyr9kqnlcEPi2LUYHoRtks"
-  # provider :twitter, ENV['SNIPLY_TWITTER_KEY'], ENV['SNIPLY_TWITTER_SECRET']
+  provider :twitter, ENV['SNIPLY_TWITTER_KEY'], ENV['SNIPLY_TWITTER_SECRET']
 end
